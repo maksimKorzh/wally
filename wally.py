3744aed3b2afdb33ce5b2d4b8a363edcfadd2f4e
Maksim Korzh <freesoft.for.people@gmail.com>
2021-12-31 13:38:17 +0200

    added 1 liberty attack/defense logic

--- wally.py
+++ wally.py
@@ -16,6 +16,8 @@
 #
 ###################################
 
+import random, sys
+
 BLACK = 1
 WHITE = 2
 
@@ -23,11 +25,11 @@ board = [
     7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
     7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
     7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 1, 0, 0, 0, 1, 0, 0, 7,
     7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
     7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
     7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
-    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
-    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7,
     7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
     7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
     7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7
@@ -35,8 +37,9 @@ board = [
 
 
 pieces = '.#o  bw +'
-liberties = 0
+liberties = []
 block = []
+best_square = 0
 
 def print_board():
     for i in range(len(board)):
@@ -61,53 +64,84 @@ def count(square, color):
         count(square + 1, color)
     elif piece == 0:
         board[square] |= 8
-        liberties += 1
+        liberties.append(square)
+
+def clear_block():
+    for captured in block:
+        board[captured] = 0
 
 def restore_board():
     global liberties, block
-    liberties = 0
+    liberties = []
     block = []
     
     for square in range(len(board)):
         if board[square] != 7: board[square] &= 3
 
 def weffect():
+    global best_square
     for square in range(len(board)):
         piece = board[square]
         if piece == 7: continue
         if piece & BLACK:
             count(square, BLACK)
-            if liberties == 0:
-                for captured in block:
-                    board[captured] = 0            
+            if len(liberties) == 0: clear_block()
+            elif len(liberties) == 1:
+                is_edge = 0
+                for d in [1, 11, -1, -11]:
+                    if board[liberties[0] + d] == 7:
+                        is_edge = 1
+                        break
+                
+                if not is_edge:
+                    board[liberties[0]] = BLACK
+                    best_square = 1
+                    break
             restore_board()
             
 def beffect():
+    global best_square
     for square in range(len(board)):
         piece = board[square]
         if piece == 7: continue
         if piece & WHITE:
             count(square, WHITE)
-            if liberties == 0:
-                for captured in block:
-                    board[captured] = 0            
+            if len(liberties) == 0: clear_block()
+            if len(liberties) == 1 and not best_square:
+                board[liberties[0]] = BLACK
+                clear_block()
+                best_square = 1
+                break
             restore_board()
 
-
 def main():
-    color = BLACK
+    global best_square
     while True:        
+        best_square = 0
         print_board()
         square_str = input('Your move: ')
         if square_str == '': continue
         _file = ord(square_str[0]) - ord('a') + 1
         _rank = 10 - (ord(square_str[1]) - ord('0'))
         square = _rank * 11 + _file
-        board[square] = color
-        color = 3 - color
+        if board[square] == 0: board[square] = WHITE
+        else: continue
+        print_board()
         weffect()
         beffect()
 
+        if best_square == 0:
+            random_square = random.randrange(len(board))
+            while board[random_square] != 0:
+                random_square = random.randrange(len(board))
+            board[random_square] = BLACK
 main()
+
+
+
+
+
+
+
 
 
